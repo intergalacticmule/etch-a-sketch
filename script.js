ef24af54c1633f0e992a86ef5459c8edfae5d2c3
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2024-01-08 23:11:06 +0200

    Finish manageEventListeners functionality

--- script.js
+++ script.js
@@ -4,31 +4,39 @@ let gridCells;
 gridContainer.style.border = "1px solid black";
 
 function createGrid(parent, size) {
-	for (let i = 1; i <= size * size; i++) {
-		const cell = document.createElement("div");
-		cell.classList.add("grid-cell");
-		cell.style.height = `${gridContainer.clientHeight / size}px`;
-		cell.style.width = `${gridContainer.clientWidth / size}px`;
-		parent.append(cell);
-	}
-	gridCells = document.querySelectorAll(".grid-cell");
+  for (let i = 1; i <= size * size; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    cell.style.height = `${gridContainer.clientHeight / size}px`;
+    cell.style.width = `${gridContainer.clientWidth / size}px`;
+    parent.append(cell);
+  }
+  gridCells = document.querySelectorAll(".grid-cell");
 }
 
 createGrid(gridContainer, 16);
 
 function setColor(e, color) {
-	e.target.style.backgroundColor = color;
+  e.target.style.backgroundColor = color;
+}
+
+function mouseoverEventListener(e) {
+  setColor(e, "black");
 }
 
 function manageEventListeners(gridCells) {
-	gridContainer.addEventListener("mousedown", (e) => {
-		setColor(e, "black");
-		console.log("alabala");
-		gridCells.forEach((cell) => {
-			cell.addEventListener("mouseover", (e) => setColor(e, "black"));
-			console.log("balaala");
-		});
-	});
+  gridContainer.addEventListener("mousedown", (e) => {
+    setColor(e, "black");
+    gridCells.forEach((cell) => {
+      cell.addEventListener("mouseover", mouseoverEventListener);
+    });
+  });
+
+  gridContainer.addEventListener("mouseup", () => {
+    gridCells.forEach((cell) => {
+      cell.removeEventListener("mouseover", mouseoverEventListener);
+    });
+  });
 }
 
 manageEventListeners(gridCells);
