d587b7fc186b391bc857baf1d0321a6903acd49a
intergalacticmule <46656364+intergalacticmule@users.noreply.github.com>
2024-01-10 11:57:36 +0200

    Implement following changes: Adjust setColors function to accomodate different modes Add adjustBorder function Rename changeColor function to changeColorFromColorPicker Add event listeners for buttons

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const gridContainer = document.querySelector("#grid-container");
 const colorPicker = document.querySelector("#color-picker");
 const rainbowButton = document.querySelector("#rainbow");
+const eraserButton = document.querySelector("#eraser");
 
 let rainbowMode = false;
+let eraserMode = false;
 let chosenColor = colorPicker.value;
 let gridCells;
 
@@ -21,20 +23,69 @@ function createGrid(parent, size) {
 createGrid(gridContainer, 16);
 
 function setColors(e, color) {
-	e.target.style.backgroundColor = color;
-	e.target.classList.add("border-none");
+	if (rainbowMode === true) {
+		e.target.style.backgroundColor = generateHexColor();
+		adjustBorder(false, e.target);
+	} else if (eraserMode == true) {
+		e.target.style.backgroundColor = "#ffffff";
+		adjustBorder(true, e.target);
+	} else {
+		e.target.style.backgroundColor = color;
+		adjustBorder(false, e.target);
+	}
+}
+
+function adjustBorder(haveBorder, target) {
+	if (haveBorder == true) {
+		target.classList.add("border-dotted");
+		target.classList.remove("border-none");
+	} else {
+		target.classList.add("border-none");
+		target.classList.remove("border-dotted");
+	}
 }
 
 function mouseoverEventListener(e) {
 	setColors(e, chosenColor);
 }
 
-function changeColor() {
+function changeColorFromColorPicker() {
+	rainbowMode = false;
 	chosenColor = colorPicker.value;
 }
 
 function toggleRainbowMode() {
-	rainbowMode = true;
+	if (rainbowMode === false) {
+		rainbowMode = true;
+		eraserMode = false;
+	} else {
+		rainbowMode = false;
+	}
+}
+
+function generateRandomNumber() {
+	return Math.floor(Math.random() * 256);
+}
+
+function generateHexColor() {
+	var hexColor = "#";
+	for (i = 0; i < 3; i++) {
+		var num = generateRandomNumber().toString(16);
+		if (num.length < 2) {
+			num = "0" + num;
+		}
+		hexColor += num;
+	}
+	return hexColor;
+}
+
+function toggleEraserMode() {
+	if (eraserMode === false) {
+		eraserMode = true;
+		rainbowMode = false;
+	} else {
+		eraserMode = false;
+	}
 }
 
 function manageEventListeners(gridCells) {
@@ -52,7 +103,8 @@ function manageEventListeners(gridCells) {
 	});
 }
 
-colorPicker.addEventListener("input", changeColor);
+colorPicker.addEventListener("input", changeColorFromColorPicker);
 rainbowButton.addEventListener("click", toggleRainbowMode);
+eraserButton.addEventListener("click", toggleEraserMode);
 
 manageEventListeners(gridCells);
